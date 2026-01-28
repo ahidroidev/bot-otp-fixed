72e302a07310fa60cff9bf0cbfbeb181c4ccc20e
Ahiezer Rada <ahidroid.exe@gmail.com>
2026-01-28 16:26:48 -0500

    Compiled Fixed

---
+++ out/next/static/23baFuv1XtqmkWyW2ezAH/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/4c5668759f17c800.js"
+  ],
+  "/_error": [
+    "static/chunks/74b85acc16eaa58e.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ out/next/static/chunks/4c5668759f17c800.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/a567dc3eef51d302.js",
+  "static/chunks/9b5e475d1f540d90.js",
+  "static/chunks/0790bcd703d52509.css",
+  "static/chunks/turbopack-a7ff120999491c98.js"
+])
